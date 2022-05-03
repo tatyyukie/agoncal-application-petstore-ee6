cd00afa7c49ca38de716f2672f74f6d8ea412a6e
Tatiana Motoyama <58003590+tatyyukie@users.noreply.github.com>
2022-05-03 19:36:36 -0300

    Utilizando @AllArgsConstructor na classe Address

--- src/main/java/org/agoncal/application/petstore/domain/Address.java
+++ src/main/java/org/agoncal/application/petstore/domain/Address.java
@@ -4,8 +4,10 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
+import lombok.AllArgsConstructor;
 
 /**
  * @author Antonio Goncalves
@@ -15,6 +17,7 @@ import lombok.Setter;
 
 @Embeddable
 @Getter @Setter
+@AllArgsConstructor
 public class Address {
 
     // ======================================
@@ -45,13 +48,6 @@ public class Address {
     // ======================================
 
     public Address() {
-    }
-
-    public Address(String street1, String city, String zipcode, String country) {
-        this.street1 = street1;
-        this.city = city;
-        this.zipcode = zipcode;
-        this.country = country;
     }
 
     // ======================================
